e0ad18105662c796ffd1ebd60da15653d10edb8e
Jameskutty Sebastian <jameskutty.sebastian@gmail.com>
2020-04-02 02:58:40 +1030

    buiid app

---
+++ build/precache-manifest.0074ded719ce935b22276f30abfa2a55.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a346a8442245990d87142943e49664a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8807aea31502d6f27a48",
+    "url": "/static/js/2.68f26e0b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.68f26e0b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c1a128da94f5866c78e5",
+    "url": "/static/js/main.b503d85a.chunk.js"
+  },
+  {
+    "revision": "355ab2a86409d6ee49ef",
+    "url": "/static/js/runtime-main.5bcffa77.js"
+  }
+]);
