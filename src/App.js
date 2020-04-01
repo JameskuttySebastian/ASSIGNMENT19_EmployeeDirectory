edd8fd45178f9782918127daf51fb7017fb57c42
Jameskutty Sebastian <jameskutty.sebastian@gmail.com>
2020-04-01 13:50:41 +1030

    added dropdown list using state

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import React, { Component } from "react";
 import employees from "../src/util/data/data.json";
 import TableData from "./components/TableData";
+import DropdownOption from "./components/DropdownOption";
 
 class App extends Component {
   state = {
     result: [],
-    searchColumn: "",
-    searchValue: ""
+    tableState: "",
+    searchColumn: "all",
+    searchColumnArray: [],
+    searchColumnChange: false,
+    searchValue: "",
+    searchValueChange: false,
+    searchValueState: ""
   };
 
   employeeListHtml = () => {
-    return this.state.result.map((employee, index) => (
+    let htmlTable = this.state.result.map((employee, index) => (
       <TableData
         key={index}
         employeeId={employee.employeeId}
@@ -21,38 +27,73 @@ class App extends Component {
         emailAddress={employee.emailAddress}
       />
     ));
+    // console.log("HTML Table: " + JSON.stringify(htmlTable));
+    this.setState({ tableState: htmlTable });
+    // console.log("Table State: " + this.state.tableState);
   };
 
-  searchColumnValue = () => {};
-
-  columnHandleChange = async event => {
-    await this.setState({ searchColumn: event.target.value });
-    this.createDropdownList();
-  };
-
-  createDropdownList = async () => {
-    let val = this.state.searchValue;
-    if (this.state.searchColumn === "all") {
-      await this.setState({ result: employees });
-    } else {
-      await this.setState({
-        result: employees.filter(
-          employee => employee.this.state.searchColumn === val
-        )
-      });
+  filterColumnChange = async event => {
+    if (event.target.value) {
+      await this.setState({ searchColumn: event.target.value });
+      this.createDropdownList();
+      //   console.log("searchColumnChange: " + this.state.searchColumnChange); // this is running after createDropdownList()
     }
   };
 
-  componentDidMount() {
-    this.setState({ result: employees });
+  filterValueChange = async event => {
+    if (event.target.value) {
+      this.setState({ searchValue: event.target.value });
+
+      if (this.state.searchColumn === "all") {
+        await this.setState({
+          result: employees,
+          searchColumnArray: [],
+          searchColumnChange: false
+        });
+      } else {
+        console.log("searchColumnChange: " + this.state.searchColumnChange);
+        await this.setState({ searchValueChange: true });
+        console.log("searchValueChange: " + this.state.searchValueChange);
+      }
+    }
+  };
+
+  createDropdownList = async () => {
+    let colValues = Array.from(
+      new Set(
+        this.state.result.map(employee => employee[this.state.searchColumn])
+      )
+    );
+    console.log(JSON.stringify(colValues));
+    await this.setState({
+      searchColumnArray: colValues,
+      searchColumnChange: true
+    });
+    this.createDropdownListMenu();
+  };
+
+  createDropdownListMenu = () => {
+    if (this.state.searchColumnArray.length) {
+      let dropdownList = this.state.searchColumnArray.map((val, index) => (
+        <DropdownOption key={index} value={val} />
+      ));
+      console.log("dropdownList Table: " + dropdownList);
+      this.setState({ searchValueState: dropdownList });
+      console.log("dropdownList State: " + this.searchValueState);
+    }
+  };
+
+  async componentDidMount() {
+    await this.setState({ result: employees });
+    this.employeeListHtml();
+    this.createDropdownListMenu();
   }
 
   render() {
     return (
       <div className="container">
         <h1>Employee Dictionary</h1>
-
-        <select id="columnList" onChange={this.columnHandleChange}>
+        <select onChange={this.filterColumnChange}>
           <option value="all">All</option>
           <option value="employeeId">Employee ID</option>
           <option value="jobTitleName">Title</option>
@@ -61,7 +102,9 @@ class App extends Component {
           <option value="phoneNumber">Phone</option>
           <option value="emailAddress">Email</option>
         </select>
-        <select id="columnValue"></select>
+        <select id="columnValue" onChange={e => this.filterValueChange(e)}>
+          {this.state.searchValueState}
+        </select>
         <table className="table table-striped">
           <thead>
             <tr>
@@ -73,7 +116,7 @@ class App extends Component {
               <th scope="col">Email</th>
             </tr>
           </thead>
-          <tbody>{this.employeeListHtml()}</tbody>
+          <tbody>{this.state.tableState}</tbody>
         </table>
       </div>
     );
--- src/components/DropdownOption.js
+++ src/components/DropdownOption.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 function DropdownOption(props) {
-  return <option value={props.value}>{props.text}</option>;
+  return <option value={props.value}>{props.value}</option>;
 }
 
 export default DropdownOption;
