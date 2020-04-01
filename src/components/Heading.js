616656c7484166c25587b48f6f4fe927c3edb7b5
Jameskutty Sebastian <jameskutty.sebastian@gmail.com>
2020-04-02 02:56:55 +1030

    build

--- src/components/Heading.js
+++ src/components/Heading.js
@@ -5,7 +5,7 @@ function Heading() {
     <div className="jumbotron jumbotron-fluid">
       <div className="container">
         <h2 className="display-4">Employee Dictionary</h2>
-        <p className="lead">Current employees database for the organisation.</p>
+        <p className="lead">Current employee database for the organisation.</p>
       </div>
     </div>
   );
